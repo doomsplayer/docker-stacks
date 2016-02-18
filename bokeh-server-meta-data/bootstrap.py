91b70c6f8a799183c78b07895c99201d49cafc1c
Wu Young <doomsplayer@gmail.com>
2016-02-18 17:29:44 +0800

    add enhanced figure

--- bokeh-server-meta-data/bootstrap.py
+++ bokeh-server-meta-data/bootstrap.py
@@ -1,4 +1,10 @@
-from bokeh.models.glyphs import Glyph
-from bokeh.core.properties import Dict, String
-class MetaGlyph(Glyph):
-    meta = Dict(String, String)
+from bokeh.plotting import Figure
+from bokeh.core.properties import Dict, String, List, Tuple, Instance
+
+class EnhancedFigure():
+    variables = Dict(String, String))
+    category = List(String)
+    plot = Instance(Figure)
+
+    def __getattr__(self, attrib):
+        return getattr(self.plot, attrib)
