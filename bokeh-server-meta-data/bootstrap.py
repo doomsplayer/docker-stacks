623893e903e2bc647f064775d5adc369fc2b449f
Wu Young <doomsplayer@gmail.com>
2016-04-12 16:23:11 +0800

    feat(bokeh-metaplot): user bokeh-metaplot

--- bokeh-server-meta-data/bootstrap.py
+++ bokeh-server-meta-data/bootstrap.py
@@ -1,13 +1,2 @@
-from bokeh.plotting import Figure
-from bokeh.core.properties import Dict, String, List
+from bokeh_metaplot import MetaChart, MetaFigure, MetaPlot
 
-class EnhancedFigure(Figure):
-    __implementation__ = """
-    Plots = require "models/plots/plot"
-    class EnhancedFigure extends Plots.Model
-        type: "EnhancedFigure"
-    module.exports =
-        Model: EnhancedFigure
-    """
-    variables = Dict(String, String)
-    category = List(String)
